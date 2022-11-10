43ce98a2d6768dc6c8b06b46401d4fbe09f8e5da
110B26021 <114041670+110B26021@users.noreply.github.com>
2022-11-10 15:51:13 +0800

    Update Class05.java

--- Class05.java
+++ Class05.java
@@ -1,17 +1,14 @@
-abstract class Math
+interface Math
  {
- protected int ans;
- public void show()
- {
- System.out.println("ans="+ans);
+ public void show();
+ public void add(int a,int b); // 計算 a+b
+ public void sub(int a,int b); // 計算 a-b
+ public void mul(int a,int b); // 計算 a*b
+ public void div(int a,int b); // 計算 a/b
  }
- public abstract void add(int a,int b); // 計算 a+b
- public abstract void sub(int a,int b); // 計算 a-b
- public abstract void mul(int a,int b); // 計算 a*b
- public abstract void div(int a,int b); // 計算 a/b
- }
- class Compute extends Math
+ class Compute implements Math
  {
+ public int ans;
  public void add(int a,int b)
  {
  ans=a+b;
@@ -28,8 +25,12 @@ abstract class Math
  {
  ans=a/b;
  }
+ public void show()
+ {
+ System.out.println("ans="+ans);
  }
- public class hw11_2
+ }
+ public class hw11_5
  {
  public static void main(String args[])
  {
